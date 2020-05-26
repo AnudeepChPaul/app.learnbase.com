5e6c573e6facbe09a72cfcc1a81b4d19de88c834
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-27 01:04:34 +0530

    Vercerl Integration

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
-const assetPrefix = process.env.BUILDING_FOR_NOW ? "/home" : "";
+// const assetPrefix = process.env.BUILDING_FOR_NOW ? "/home" : "";
 
 module.exports = {
-  assetPrefix,
-  env: {
-    ASSET_PREFIX: assetPrefix,
+  assetPrefix
+  , env: {
+    AUTH_SUCCESS_URL: ''
   }
-  // distDir: "build",
-  // exportTrailingSlash: true,
-  // exportPathMap: async function (
-  //   defaultPathMap,
-  //   { dev, dir, outDir, distDir, buildId }
-  // ) {
-  //   const pathMap = {
-  //     "/": { page: "/" },
-  //     "/about": { page: "/about" },
-  //     "/modules": { page: "/modules" },
-  //     "/modules/**": { page: "/modules" },
-  //   };
-
-  //   return pathMap;
-  // },
 };
