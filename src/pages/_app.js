8b83e4e8bfc37c5374d8a087c48ac36994998bbb
Anudeep Chandra Paul <achandrapaul@manh.com>
2020-05-29 00:18:19 +0530

    [Route] - Server redirect to home

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,25 +4,40 @@ import App from 'next/app'
 import MainLayout from '@/Layouts/Main/MainLayout'
 import api from '@/Api/Api'
 
+export function redirectUser(ctx, location) {
+  if (ctx.res && typeof ctx.res.writeHead === "function") {
+    ctx.res.writeHead(302, { Location: location });
+    ctx.res.end();
+  }
+}
+
 export default class MyApp extends App {
-  static async getInitialProps({ Component, router, ctx }) {
-    const res = await api().get('/getAppProperties')
-    const data = JSON.parse(JSON.stringify(res.data))
+    static async getInitialProps({Component, router, ctx}) {
+        const res = await api().get('/getAppProperties');
+        const data = JSON.parse(JSON.stringify(res.data));
+        let pageProps = {};
 
-    return {
-      pageProps: {
-        appProps: data
-      }
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+
+        pageProps.appProps = data;
+
+        if (ctx.pathname === "/" || ctx.pathname === "/_error") {
+            redirectUser(ctx, "/home");
+            return {pageProps};
+        }
+
+        return {pageProps}
     }
-  }
 
-  render() {
-    const { Component, pageProps } = this.props;
+    render() {
+        const {Component, pageProps} = this.props;
 
-    return (
-      <MainLayout {...pageProps.appProps}>
-        <Component {...pageProps}></Component>
-      </MainLayout>
-    )
-  }
+        return (
+            <MainLayout {...pageProps.appProps}>
+                <Component {...pageProps}></Component>
+            </MainLayout>
+        )
+    }
 }
